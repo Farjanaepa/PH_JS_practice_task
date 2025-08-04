d8d520eb1969ee95df9fb69ba42260358dd34078
Farjanaepa <farjanaepa@gmail.com>
2025-08-04 22:18:21 +0600

    function added and jog and vag complete task

---
+++ function.js
@@ -0,0 +1,21 @@
+function gunerMatchine(input1,input2) {
+    let multiplication=input1*input2;
+    console.log("multiple = " + multiplication);
+    return multiplication;
+    
+}
+const result=gunerMatchine(10,40);
+console.log(result);
+
+// vag er
+
+function vagermatchine(a,b) {
+    let divided=a/b;
+    console.log("divided = " + divided);
+    return divided;
+    
+}
+const devideresult=vagermatchine(25,125);
+console.log(devideresult);
+
+
