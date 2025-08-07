b970c290d24e87f3758d37cb8f71e90beb3e769a
Farjanaepa <152874011+Farjanaepa@users.noreply.github.com>
2025-08-07 11:42:35 +0600

    Create arrayofObject access object inside an array

---
+++ arrayofObject.js
@@ -0,0 +1,11 @@
+// access object inside the array
+
+const num = [1,2,4,6];
+const players = [{},{},{}];
+
+
+const employes = [
+    {name:'asik', designation:'developer', salary: 25000},
+    {name:'atik', designation: 'maketing', salary: 21000}
+]
+console.log(employes[0].designation);
