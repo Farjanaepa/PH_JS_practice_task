354df356af4d96cffc09c7cfd8de3a5c862356dc
Farjanaepa <farjanaepa@gmail.com>
2025-08-08 20:28:04 +0600

    function parameter

---
+++ functionparameter.js
@@ -0,0 +1,16 @@
+// function strucure
+
+// function name() {
+//     console.log('bosa theke uth');
+//     console.log('walk');
+//     console.log('click the switch');
+// }
+// // call the function
+// name();
+
+function square (number){
+    console.log(number);
+    const borgo = number * number;
+    console.log('square number :', borgo)
+}
+square(45);
