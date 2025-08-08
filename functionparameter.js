ec3110f76691eb1b25b79d5a1dc9bf94dd78c155
Farjanaepa <farjanaepa@gmail.com>
2025-08-09 00:22:09 +0600

    function multiple parameter

--- functionparameter.js
+++ functionparameter.js
@@ -14,3 +14,10 @@ function square (number){
     console.log('square number :', borgo)
 }
 square(45);
+
+
+function addAll(a,b,c,d,e) {
+    const total = a+b+c+d+e;
+    console.log(total)
+}
+addAll(2,4,5,3,4);
