865c9bce81b49a3131a2298efac8e9d4e4623c82
Farjanaepa <farjanaepa@gmail.com>
2025-08-10 13:42:32 -0300

    js problem part 2 max number

--- js-problem-part2/max.js
+++ js-problem-part2/max.js
@@ -6,4 +6,12 @@ function maxNumber(num1,num2){
     }
 }
 const max = maxNumber(15,49);
+const max2 = maxNumber(45,90);
+const ultimatemax = maxNumber(max,max2);
+console.log(ultimatemax);
 console.log(max);
+
+// 100/200 student e compare korle
+
+const maximum = Math.max(23,67,56,45,90,890,76);
+console.log(maximum);
