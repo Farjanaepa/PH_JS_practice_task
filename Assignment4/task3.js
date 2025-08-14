ff6c4bf69a59c61ddfeef2ac675d12c2c8277bee
Farjanaepa <farjanaepa@gmail.com>
2025-08-14 22:09:03 +0600

    task5 complete

--- Assignment4/task3.js
+++ Assignment4/task3.js
@@ -2,7 +2,7 @@ function  bestTeam( player1, player2 ) {
       // You have to write your code here
    
      
-    if(typeof player1 !== "object" || player1 === null || typeof player2 !== "object" || player2 === null) {
+    if(typeof player1 !== "object" && player1 === null || typeof player2 !== "object" || player2 === null) {
         return "invalid"
     }
     
@@ -18,10 +18,10 @@ function  bestTeam( player1, player2 ) {
 
     } else if(player1_total === player2_total) {
         return "tie"
-    } 
+    } else {
+        return "Invalid";
+    }
 
 }
-const result=bestTeam({ name: "Germany", foul: 1, cardY: 0, cardR: 0 },
-{ name: "Sweden", foul: 7, cardY: 4, cardR: 1 }
-);
+const result=bestTeam({},{});
 console.log(result);
---
+++ Assignment4/task4.js
@@ -0,0 +1,21 @@
+function  isSame(arr1 , arr2 ) {
+     // You have to write your code here
+     if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+        return "invalid";
+     }else if (arr1.length !== arr2.length) {
+        return false;
+     }
+     for(let i = 0; i<arr1.length; i++){
+        if(arr1[i] !==  arr2[i]){
+            return false;
+        } 
+     }
+     return true;
+}
+const result =isSame([1 , "4" , 4] ,[1 , 4  , 4]
+
+
+);
+console.log(result);
+
+
