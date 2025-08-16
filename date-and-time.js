f89381c3893ebd7979a288ad166a3b2bc2c9841d
Farjanaepa <farjanaepa@gmail.com>
2025-08-16 23:02:43 +0600

    date and month calculation

--- date-and-time.js
+++ date-and-time.js
@@ -3,3 +3,9 @@ const date = new Date ('2062-10-19')
 
 console.log(date.getMonth());
 console.log(date.getDay());
+
+const specificDate= new Date(2091,0,26);
+console.log(specificDate);
+specificDate.setMonth(10);
+console.log(specificDate.toLocaleString('en-GB'));
+// unix epoc - ekta time theke bortoman porjonto koto somoy ber korar jonno use hoy
