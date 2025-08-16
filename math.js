9d2c7f63bd2ea75070ad81e4dca461f8ff146272
Farjanaepa <farjanaepa@gmail.com>
2025-08-16 22:51:31 +0600

    math formula try

--- math.js
+++ math.js
@@ -3,3 +3,20 @@ console.log(min);
 
 const max = Math.max(56,57,34,90,99);
 console.log(max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(5-12));
+
+console.log(Math.round(2.85));  /*[nearest number] -> output : 3 */
+
+console.log(Math.floor(2.9995)); /* remove point only before digit -> output :2 */
+
+console.log(Math.ceil(2.00001)) /* next number -> output : 3 */
+
+console.log(Math.random());
+
+console.log(Math.random()*10); /* 10 er moddhe jekono sonkha dibe */
+
+const rand = Math.round(Math.random()*10);
+console.log(rand);
