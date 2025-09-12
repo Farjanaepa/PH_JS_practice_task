01545a16bdea6f8791e6a68d09e1915373b2caed
Farjanaepa <farjanaepa@gmail.com>
2025-09-12 21:54:32 +0600

    async sync practice

---
+++ more-js-concepts/async.js
@@ -0,0 +1,21 @@
+
+// syncronus
+console.log('one');
+console.log('two');
+// callThree()
+
+setTimeout(()=> {
+    console.log('new three inside set time out')
+},4000)
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+console.log('four');
+console.log('five');
+console.log('six');
+console.log('seven');
+
+// function callThree(){
+// console.log('three');
+
+// }
